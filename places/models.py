533eef35c6a5fe39c1c374863e8e7ee6858f2277
ludwig300 <mr4silent@gmail.com>
2023-05-04 01:00:17 +0700

    Add model for images

--- places/models.py
+++ places/models.py
@@ -10,3 +10,19 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Image(models.Model):
+    photo = models.ImageField(
+        upload_to='images',
+        verbose_name='Изображение',
+        null=True
+    )
+    place = models.ForeignKey(
+        Place, on_delete=models.CASCADE,
+        related_name='image',
+        verbose_name='Место'
+    )
+
+    def __str__(self):
+        return f'{self.id} {self.place.title}'
