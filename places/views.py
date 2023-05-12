a7b2860514c010ab0516e9948a1f6555976b5d29
ludwig300 <mr4silent@gmail.com>
2023-05-12 18:18:10 +0700

    Added place detail view

--- places/views.py
+++ places/views.py
@@ -1,6 +1,7 @@
 import json
 
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
 from .models import Place
@@ -28,3 +29,18 @@ def index(request):
     context = {'places_data': json.dumps(places_data)}
 
     return render(request, 'index.html', context)
+
+
+def place_detail_view(request, placeId):
+    place = get_object_or_404(Place, placeId=placeId)
+    place_data = {
+        "title": place.title,
+        "imgs": [request.build_absolute_uri(img.photo.url) for img in place.images.all()],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.lng,
+            "lat": place.lat
+        }
+    }
+    return JsonResponse(place_data, json_dumps_params={'ensure_ascii': False, 'indent': 4})
