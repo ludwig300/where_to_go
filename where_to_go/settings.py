6cbd90535113252e8665cc9476683b80ea8c4044
ludwig300 <mr4silent@gmail.com>
2023-05-13 14:16:13 +0700

    Added env to settings

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -1,20 +1,42 @@
 import os
 from pathlib import Path
-
+from environs import Env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = Env()
+env.read_env()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+SECRET_KEY = env.str('SECRET_KEY')
+DEBUG = env.bool('DEBUG', default=False)
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7m=uc)vh5-q(tn^smym=$xux_u!*!1z8))h307y_@8-i=$%5bv'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, env.str('DB_NAME', 'db.sqlite3')),
+    }
+}
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+LANGUAGE_CODE = env.str('LANGUAGE_CODE', 'en-us')
+TIME_ZONE = env.str('TIME_ZONE', 'UTC')
+USE_I18N = env.bool('USE_I18N', True)
+USE_TZ = env.bool('USE_TZ', True)
 
-ALLOWED_HOSTS = []
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CKEDITOR_UPLOAD_PATH = 'uploads/ckeditor/'
+
+SECURE_HSTS_SECONDS = env.int('SECURE_HSTS_SECONDS', 31536000)
+SECURE_SSL_REDIRECT = env.bool('SECURE_SSL_REDIRECT', True)
+SESSION_COOKIE_SECURE = env.bool('SESSION_COOKIE_SECURE', True)
+CSRF_COOKIE_SECURE = env.bool('CSRF_COOKIE_SECURE', True)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool('SECURE_HSTS_INCLUDE_SUBDOMAINS', True)
+SECURE_HSTS_PRELOAD = env.bool('SECURE_HSTS_PRELOAD', True)
 
 
 # Application definition
@@ -62,18 +84,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'where_to_go.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -91,30 +101,3 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CKEDITOR_UPLOAD_PATH = 'uploads/ckeditor/'
