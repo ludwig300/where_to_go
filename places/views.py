6a6e964b7014792cfddf94ab306dfd63e81693fc
ludwig300 <mr4silent@gmail.com>
2023-05-17 23:11:56 +0700

    Renamed variable

--- places/views.py
+++ places/views.py
@@ -7,8 +7,8 @@ from django.urls import reverse
 from .models import Place
 
 
-def place_detail_view(request, id):
-    place = get_object_or_404(Place, id=id)
+def place_detail_view(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
     place_data = {
         'title': place.title,
         'imgs': [request.build_absolute_uri(img.photo.url) for img in place.images.all()],
